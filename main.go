b441efef43ebf7fd416b40c196701d5d2f9a7e5a
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-23 00:42:42 +0500

    root hanlder added for "/"

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 	"os"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Running API v1\n"))
+}
+
 func main() {
+	http.HandleFunc("/", rootHandler)
 	err := http.ListenAndServe("localhost:4000", nil)
 	if err != nil {
 		fmt.Println(err)
