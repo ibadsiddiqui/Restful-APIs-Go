55f226f5404fb01f68ec601b80f5a4ddae1a76dd
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-23 13:02:40 +0500

    service for fetching all users from database added

--- user/user.go
+++ user/user.go
@@ -1,18 +1,35 @@
 package user
 
 import (
-	"errors"
+	// "errors"
 
 	"gihub.com/asdine/storm"
 	"gopkg.in/mgo.v2/bson"
 )
 
 type User struct {
-	ID		bson.ObjectId 	`json:"id" storm:"id"`
-	Name 	string			`json:"name"`
-	Role 	string			`json:"role"`
+	ID   bson.ObjectId `json:"id" storm:"id"`
+	Name string        `json:"name"`
+	Role string        `json:"role"`
 }
 
 const dbPath = "users.db"
 
 var ErrRecordInvalid = error.New("Record is invalid")
+
+// All retrieves all users from the database
+func All() ([]User, error) {
+	db, err := storm.Open(dbPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	users := []User{}
+	err = db.All(&users)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
