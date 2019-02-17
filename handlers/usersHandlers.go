73aeffe68f08ff795f1bae954d10545d7c8a8a4d
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-17 23:46:35 +0500

    patch service method added for patching existing user data

--- handlers/usersHandlers.go
+++ handlers/usersHandlers.go
@@ -15,7 +15,6 @@ import (
 func bodyToUser(r *http.Request, u *user.User) error {
 	if r.Body == nil {
 		return errors.New("request body is empty")
-
 	}
 	if u == nil {
 		return errors.New("a user is required")
@@ -80,6 +79,37 @@ func usersGetOne(w http.ResponseWriter, _ *http.Request, id bson.ObjectId) {
 func usersPutOne(w http.ResponseWriter, r *http.Request, id bson.ObjectId) {
 	u := new(user.User)
 	err := bodyToUser(r, u)
+	if err != nil {
+		postError(w, http.StatusBadRequest)
+		return
+	}
+
+	u.ID = id
+	err = u.Save()
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			postError(w, http.StatusBadRequest)
+		} else {
+			postError(w, http.StatusInternalServerError)
+		}
+		return
+	}
+
+	postBodyResponse(w, http.StatusOK, jsonResponse{"user": u})
+}
+
+func usersPatchOne(w http.ResponseWriter, r *http.Request, id bson.ObjectId) {
+	u, err := user.One(id)
+
+	if err != nil {
+		if err != storm.ErrNotFound {
+			postError(w, http.StatusNotFound)
+			return
+		}
+		postError(w, http.StatusInternalServerError)
+		return
+	}
+	err = bodyToUser(r, u)
 	if err != nil {
 		postError(w, http.StatusBadRequest)
 		return
